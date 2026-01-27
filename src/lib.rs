9b60af34daf584ff4fad483ebc6570c47c0bea17
mompei <ogunborjerry@gmail.com>
2026-01-27 05:39:12 -0800

    The real end of the basic health check api

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,16 @@
-//! lib.rs
+//! src/lib.rs
+use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
-
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
-
-// Accept address as a parameter so tests can use different ports
-pub async fn run(address: &str) -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind(address)?
-    .run()
-    .await
+// Notice the different signature!
+// We return `Server` on the happy path and we dropped the `async` keyword
+// We have no .await call, so it is not needed anymore.
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run();
+    // No .await here!
+    Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-//! main.rs
+//! src/main.rs
 use zero2prod::run;
-
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run("127.0.0.1:8000").await
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    run()?.await
 }
